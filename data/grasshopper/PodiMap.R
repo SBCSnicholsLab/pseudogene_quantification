45a457e8af380cf098b3fb696c36e0881c5261d2
Hannes Becher <hannesbecher@gmx.de>
2022-10-24 09:18:03 +0100

    relevant information

--- data/grasshopper/PodiMap.R
+++ data/grasshopper/PodiMap.R
@@ -1,14 +1,14 @@
-# This is the code for the raw version of the Podisma map (figure 3b)
-setwd("~/git_repos/pseudogene_quantification/data/grasshopper/")
-sampDat <- read.csv("sampleData.csv", sep='\t')
+
+setwd("~/Dropbox/Richard_share/mtAnalysis2020/@manuscript/Figures/")
+sampDat <- read.csv("~/git_repos/pseudogene_quantification/data/grasshopper/sampleData.csv", sep='\t')
 str(sampDat)
-plot(sampDat$Long, sampDat$Lat, col = sampDat$Karyotype, pch=as.numeric(sampDat$Mitotype))
+plot(sampDat$Long, sampDat$Lat, pch=as.numeric(as.factor(sampDat$Mitotype)))
 
 # available from https://drive.google.com/drive/folders/17dnXkQKlF_fcqqETrHco5cVfF3R7kty0
 alt <- read.table("~/Dropbox/elevations/srtm_38_04.asc.gz", sep = " ", skip = 6)
 alt <- as.matrix(alt)
 
-library(rgl)
+#library(rgl)
 
 laMin <- 44.2
 laMax <- 44.6
@@ -26,7 +26,8 @@ ymin <- abs(floor((laMin - yllcorner)/ cellsize) - 6001)
 ymax <- abs(ceiling((laMax - yllcorner)/ cellsize) - 6001)
 
 area <- t(alt)[xmin:xmax, ymin:ymax]
-image(area, col=terrain.colors(20))
+
+image(area, col=c("#777777", "#888888", "#AAAAAA", "#CCCCCC"), breaks=c(0,1000,1500, 2000, 5000))
 contour(area, add=T, levels = c(1000,1500,2000))
 
 laConvert <- function(x){
@@ -40,18 +41,31 @@ laConvert(seq(laMin, laMax, 0.1))
 #pdf("PodiMap.pdf")
 image(area, col=grey.colors(20)[3:20],
       xaxt = 'n',
-      yaxt = 'n', 
+      yaxt = 'n',
       xlab="Longitude",
       ylab="Latitude",
       asp=(laMax-laMin)/(loMax-loMin),
       frame = F)
 
-contour(area, add=T, levels = c(1000,1500,2000), col = "#333333")
+#png("PodiMapBlank.png", width=4000, height=4000)
+image(area, col=c("#777777", "#888888", "#AAAAAA", "#CCCCCC"), breaks=c(0,1000,1500, 2000, 5000),
+      xaxt = 'n',
+      yaxt = 'n', 
+      xlab="Longitude",
+      ylab="Latitude",
+      asp=(laMax-laMin)/(loMax-loMin),
+      frame = F)
+contour(area, add=T, levels = c(1000,1500,2000), col = "#111111", lwd=10, labcex = 5)
+#dev.off()
 axis(1, at = loConvert(seq(loMin, loMax, 0.1)), labels = seq(loMin, loMax, 0.1)) # at goes from 0 to 1 in image
 axis(2, at = laConvert(seq(laMin, laMax, 0.1)), labels = seq(laMin, laMax, 0.1)) # at goes from 0 to 1 in image
-combs <- interaction(sampDat$Mitotype, sampDat$Karyotype)
-points(loConvert(sampDat$Long), laConvert(sampDat$Lat), pch = as.numeric(combs), lwd=2)
-legend("topright", pch=1:6, legend = levels(combs))
-#dev.off()
-table(sampDat$Karyotype)
 
+points(loConvert(sampDat$Long), laConvert(sampDat$Lat), pch = as.numeric(combs), lwd=2)
+legend("topright", pch=1:6, legend = levels(as.factor(sampDat$Mitotype)))
+dev.off()
+
+
+#pdf("contours.pdf")
+plot(c(0,0), c(1,1), type='n', xlim=c(0,1), ylim=c(0,1),asp=(laMax-laMin)/(loMax-loMin))
+contour(area, add=T, levels = c(1000,1500,2000), col = "#111111")
+#dev.off()
